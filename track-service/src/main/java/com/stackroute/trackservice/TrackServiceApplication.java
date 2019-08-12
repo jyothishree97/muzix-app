6480e6e7f2321a00a0a785fca9b10ab48d3c2899
jyothishree.a <jyothishree.a@cgi.com>
2019-08-12 09:36:16 +0530

    docker-compose completed

--- track-service/src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
+++ track-service/src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
@@ -3,10 +3,12 @@ package com.stackroute.trackservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
+@EnableDiscoveryClient
 //@EnableSwagger2 annotation is used to enable the Swagger2 for our Spring Boot application.
 @SpringBootApplication
 @PropertySource("classpath:application-prod.properties")
